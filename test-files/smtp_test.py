1b3ac25fb32ad40ccb0a4c7451667c96fc160074
Syed Arbaaz Hussain <69892240+SyedArbaazHussain@users.noreply.github.com>
2024-10-21 06:58:31 -0700

    Feature : File upload (#1)
    * Implement File Upload
    * Remove unnecessary Files
    * Update .gitignore

---
+++ test-files/smtp_test.py
@@ -0,0 +1,28 @@
+import smtplib
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+
+load_dotenv()
+# Create a message
+subject = 'Fall Detected'
+body = 'Fall Detected!! Ceck your app for more details'
+
+msg = MIMEMultipart()
+msg['From'] = os.environ.get('SENDER_EMAIL')
+msg['To'] = os.environ.get('RECIEVER_EMAIL')
+msg['Subject'] = subject
+msg.attach(MIMEText(body, 'plain'))
+
+# Send the email
+try:
+    server = smtplib.SMTP(os.environ.get('SMTP_SERVER'), os.environ.get('SMTP_PORT'))
+    server.starttls()  # Upgrade the connection to a secure encrypted SSL/TLS connection
+    server.login(os.environ.get('SENDER_EMAIL'), os.environ.get('SENDER_PASSWORD'))
+    server.send_message(msg)
+    print('Email sent successfully!')
+except Exception as e:
+    print(f'Error occurred: {e}')
+finally:
+    server.quit()
